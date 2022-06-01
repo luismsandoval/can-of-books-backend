076ca7fa2ff17432d4c6ed49128ded6d2f655925
Luis Sandoval <luismsandoval96@gmail.com>
2022-05-31 17:54:53 -0700

    added mongoose to server and created schema model for books data

--- server.js
+++ server.js
@@ -3,16 +3,38 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Book = require('./books');
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+// connect mongoDB via mongoose
+mongoose.connect(process.env.MONGODB_URI);
 
-  response.send('test request received')
+// error handling via mongoose
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected')
+});
+
+app.get('/', (req, res) => {
+
+  res.send('test request received')
 
 })
+
+app.get('/books', async (req, res) => {
+  const books = await Book.find({});
+
+  //how to send a query
+  // const books = await Book.find({status: 'available'});
+  // const books = await Book.find({status: req.query.status});
+
+  res.send(books);
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
